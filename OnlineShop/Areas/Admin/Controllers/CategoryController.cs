1e2e349f877d5cf0d75d41efc3e965c7d6bfc344
TIENTRAN-PC\tranv <tranviettien1996@gmail.com>
2017-10-22 23:11:54 +0700

    Create category controller

---
+++ OnlineShop/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,135 @@
+﻿using OnlineShop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OnlineShop.Areas.Admin.Controllers
+{
+    public class CategoryController : AdminController
+    {
+        // GET: Admin/Category
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult GetCategories()
+        {
+            try
+            {
+                var data = DbContext.Categories.Where(c => c.Status == true).ToList();
+                var response = data.Select(x => new {
+                    Id = x.Id,
+                    Name = x.Name,
+                    //Producers = x.CategoryProducers.Select(p => new {
+                    //    //producerId = p.Producer.Id,
+                    //    producerName = p.Producer.Name
+                    //} )
+                    Producers = new int[] { 1, 2, 3 }
+                });
+               return Json(response, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
+        [HttpPost]
+        public JsonResult Add(string name)
+        {
+            if (!string.IsNullOrEmpty(name))
+            {
+                try
+                {
+                    var cate = new Category();
+                    cate.Name = name;
+                    var data = DbContext.Categories.Add(cate);
+                    DbContext.SaveChanges();
+                    return Json(new
+                    {
+                        result = "Success",
+                        data = data
+                    });
+                }
+                catch (Exception e)
+                {
+                    return Json(new
+                    {
+                        result = "Fail",
+                        error = e.ToString()
+                    });
+                }
+            }
+            return null;
+
+        }
+
+        [HttpPost]
+        public JsonResult Update(int id, string name)
+        {
+            if (!string.IsNullOrEmpty(name))
+            {
+                try
+                {
+                    var cate = DbContext.Categories.Find(id);
+                    if (cate != null)
+                    {
+                        cate.Name = name;
+                        DbContext.SaveChanges();
+                        return Json(new
+                        {
+                            result = "Success",
+                            data = cate
+                        });
+                    }
+                    
+                }
+                catch (Exception e)
+                {
+                    return Json(new
+                    {
+                        result = "Fail",
+                        error = e.ToString()
+                    });
+                }
+            }
+            return null;
+
+        }
+
+        [HttpGet]
+        public JsonResult Delete(int id)
+        {
+            if (id != null)
+            {
+                try
+                {
+                    var cate = DbContext.Categories.Find(id);
+                    if (cate != null)
+                    {
+                        cate.Status = false;
+                        DbContext.SaveChanges();
+                        return Json(new
+                        {
+                            result = "Success",
+                            data = cate
+                        });
+                    }
+                }
+                catch (Exception e)
+                {
+                    return Json(new
+                    {
+                        result = "Fail",
+                        error = e.ToString()
+                    });
+                }
+            }
+            return null;
+
+        }
+    }
+}
---
+++ OnlineShop/Areas/Admin/javascript/category/index.js
@@ -0,0 +1,49 @@
+﻿$(document).ready(function () {
+    table = $('#myGrid').DataTable({
+        dom: 'l<"br">Bfrtip',
+
+        "ajax": {
+            "url": "/Admin/Category/GetCategories/",
+        },
+        "columns": [
+            { "data": "Id" },
+            { "data": "Name" },
+            { "data": "Producers[, ]" }
+        ],
+        buttons: [
+            {
+                text: 'Thêm <i class="fa fa fa-plus"></i>',
+                action: function (e, dt, node, config) {
+                    $('#AddForm')[0].reset();
+                    $('#AddModal').modal('show');
+                }
+            }
+        ],
+        initComplete: function (data) {
+            $("div.br").html('<br></br>');
+            
+        }
+    });
+
+    // Edit record
+    $('#myGrid').on('click', 'i.fa-edit', function (e) {
+        e.preventDefault();
+        var data = table.row($(this).parents('tr')).data();
+        id = data.Id;
+        $('#Updatename').val(data.Name);
+        data.Image != null ? $('#Updateimage').attr("src", "../Photos/Producer/" + data.Image) : "";
+        document.getElementById('Updateimage2').value = "";
+        $('#EditAlertModal').modal('show');
+    });
+
+    // Delete a record
+    $('#myGrid').on('click', 'i.fa-trash-o', function (e) {
+        e.preventDefault();
+        var data = table.row($(this).parents('tr')).data();
+        id = data.Id;
+        $('#DeleteAlertModal').modal('show');
+
+
+    });
+
+});
