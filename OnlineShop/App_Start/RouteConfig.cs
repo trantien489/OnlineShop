7a40daf2fec5749e31fd5049e8188dc1401a496c
TIENTRAN-PC\tranv <tranviettien1996@gmail.com>
2017-11-15 00:45:36 +0700

    User add navigation bar

--- OnlineShop/App_Start/RouteConfig.cs
+++ OnlineShop/App_Start/RouteConfig.cs
@@ -16,7 +16,9 @@ namespace OnlineShop
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "HomePage", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "HomePage", id = UrlParameter.Optional },
+                namespaces: new[] { "OnlineShop.Controllers" }
+
             );
 
         }
